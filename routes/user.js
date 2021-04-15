ef082b802e4a1d78bca1d286ba869270e017a76e
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-04-15 19:08:54 +0530

    Added saved parking feature! Project done, Hurray!

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { signin, checkToken } = require("../controllers/auth");
-const { getUserById, addUser, updateUserById } = require("../controllers/user");
+const { getUserById, addUser, updateUserById, addParkingPointToSaved } = require("../controllers/user");
 
 // creating a user
 router.post("/", addUser);
+
+// saving Parking Point to user account
+router.post("/saveparkingpoint", checkToken, addParkingPointToSaved);
 
 // get a user's data
 router.get("/user/", checkToken, getUserById);
